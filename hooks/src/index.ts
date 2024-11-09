12777aed17910c0659a0d1b44b22ad1218feff35
Gagan Sharma <sharmagagan192@gmail.com>
2024-11-10 00:12:38 +0530

    Store trigger in DB

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,37 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-const app = express();
 
-app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
+const client = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT;
+app.post("/hooks/catch/:userId/:zapId",async (req,res)=>{
     const userId = req.params.userId;
     const zapId = req.params.zapId;
+    const body = req.body;
 
     //store in db a new trigger
+    await client.$transaction(async tx =>{
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+
+        await tx.zapRunOutbox.create({
+            data:{
+                runId: run.id
+            } as any
+        });
+
+        res.json({
+            message: "Webhook Received"
+        });
+    })
 
     //push in to the queue (kafka/redis)
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is listening at ${PORT}`)
 })
